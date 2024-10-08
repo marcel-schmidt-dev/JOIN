42ca510aa56ec53174e7adf66ac1e60e8bf8a1a3
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-08 15:43:26 +0200

    Refactor returnContactList function to display a sorted contact list with alphabetical headers

--- components/contacts/contact-list/contact-list.js
+++ components/contacts/contact-list/contact-list.js
@@ -1,7 +1,33 @@
-export function returnContactList(contactList) {
+import { getContacts } from "../../firebase.js";
+
+export default async function returnContactList() {
+
+    const contactList = Object.values(await getContacts());
+
     return /*html*/`
         <div class="contact-list">
             <button>Add new contact</button>
+            <div class="list">
+                ${returnContactListTemplate(contactList)}
+            </div>
         </div>
     `
+}
+
+function returnContactListTemplate(contactList) {
+    let currentLetter = '';
+    let htmlList = '';
+
+    const sortedContacts = contactList.sort((a, b) => a.fullName.localeCompare(b.fullName));
+
+    sortedContacts.forEach(contact => {
+        const firstLetter = contact.fullName.charAt(0).toUpperCase();
+        if (firstLetter !== currentLetter) {
+            currentLetter = firstLetter;
+            htmlList += `<div class="letter">${currentLetter}</div>`;
+        }
+        htmlList += `<div class="contact">${contact.fullName}</div>`;
+    });
+
+    return htmlList;
 }
