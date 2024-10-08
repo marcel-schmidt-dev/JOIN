eaa87b706e341e24640a7afbbbca7a5c39a0ff20
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-08 16:26:38 +0200

    Refactor file name and export function in utility-functions.js

--- components/utility-functions.js
+++ components/utility-functions.js
@@ -1,4 +1,4 @@
-export default function returnRandomUserColor() {
+export function returnRandomUserColor() {
     const colors = ['#0038FF', '#00BEE8', '#1FD7C1', '#6E52FF', '#9327FF', '#9747FF', '#C3FF2B', '#FC71FF', '#FF4646', '#FF5EB3', '#FF745E']
     return colors[Math.floor(Math.random() * colors.length)]
 }
