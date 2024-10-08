2489649f02e9a80dd8c9d6267350b54e418139f8
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-08 17:46:03 +0200

    Refactor utility-functions.js to add getInitialsFromName function

--- components/utility-functions.js
+++ components/utility-functions.js
@@ -2,3 +2,10 @@ export function returnRandomUserColor() {
     const colors = ['#0038FF', '#00BEE8', '#1FD7C1', '#6E52FF', '#9327FF', '#9747FF', '#C3FF2B', '#FC71FF', '#FF4646', '#FF5EB3', '#FF745E']
     return colors[Math.floor(Math.random() * colors.length)]
 }
+
+export function getInitialsFromName(fullName) {
+    const nameParts = fullName.split(" ");
+    const firstNameInitial = nameParts[0].charAt(0).toUpperCase();
+    const lastNameInitial = nameParts.length > 1 ? nameParts[1].charAt(0).toUpperCase() : '';
+    return firstNameInitial + lastNameInitial;
+}
