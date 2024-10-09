069acebb6671b4bde57c6f98436c99e9b200b1b8
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-09 19:51:14 +0200

    Refactor contact list

--- components/contacts/contacts.js
+++ components/contacts/contacts.js
@@ -1,14 +1,26 @@
-import returnContactList from "./contact-list/contact-list.js";
+import returnContactListTemplate from "./contact-list/contact-list.js";
+import { getContacts } from "../firebase.js";
 import getContactInfosTemplate from "./contact-details/contact-details.js";
+
+window.showContactDetails = showContactDetails;
+
+let contactList;
 
 document.addEventListener("DOMContentLoaded", async function () {
   const contentRef = document.querySelector(".content");
+  contactList = await getContacts();
 
-  contentRef.innerHTML += await returnContactList();
-  contentRef.innerHTML += getContactInfosTemplate({
-    fullName: "John Doe",
-    email: "john.doe@example.com",
-    phone: "+1-202-555-0175",
-    userColor: "#4a5a7f",
-  });
+  contentRef.innerHTML += returnContactListTemplate(contactList);
 });
+
+export function showContactDetails(id) {
+
+  const contact = contactList.find(contact => contact.id === id);
+
+  const contentRef = document.querySelector(".content");
+  const contactSectionRef = contentRef.querySelector('.contact-section');
+  if (contactSectionRef) contactSectionRef.remove();
+
+  contentRef.innerHTML += getContactInfosTemplate(contact);
+
+}
