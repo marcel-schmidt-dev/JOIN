ee4a9321f83850f9227d85ae882f1c77e34e3228
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-09 22:35:07 +0200

    Refactor utility-functions.js: Update returnRandomUserColor() color array

--- components/utility-functions.js
+++ components/utility-functions.js
@@ -1,5 +1,5 @@
 export function returnRandomUserColor() {
-    const colors = ['#0038FF', '#00BEE8', '#1FD7C1', '#6E52FF', '#9327FF', '#9747FF', '#C3FF2B', '#FC71FF', '#FF4646', '#FF5EB3', '#FF745E']
+    const colors = ['#0038FF', '#00BEE8', '#1FD7C1', '#6E52FF', '#9327FF', '#9747FF', '#FC71FF', '#FF4646', '#FF5EB3', '#FF745E']
     return colors[Math.floor(Math.random() * colors.length)]
 }
 
