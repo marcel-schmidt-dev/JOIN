1f2272d8a842e09da3b2bd2a84e1106d9b65c269
Philipp Anstett <phil.anstett@icloud.com>
2024-10-13 20:44:26 +0200

    form-validation updated

--- components/contacts/contact-overlay/contact-overlay.js
+++ components/contacts/contact-overlay/contact-overlay.js
@@ -32,20 +32,28 @@ export default function getContactOverlayTemplate(contactInfos) {
             </div>
           </div>
           <div>
-            <p id="name-requested">Vollständiger Name erforderlich</p>
-            <div class="input-container">
+            <div class="input-content">
+            <div class="input-container" id="input-container-name">
               <input id="name" type="text" name="name" placeholder="Name" value="${contactInfos ? contactInfos.fullName : ""}" />
               ${returnIcon("user-outline")}
             </div>
-            <p id="email-requested">Gültige Email-Adresse erforderlich</p>
-            <div class="input-container">
+            <div class="request-container">
+              <p id="name-requested">Vollständiger Name erforderlich</p>
+             </div>
+            </div>
+            <div class="input-content">
+            <div class="input-container" id="input-container-email">
               <input id="email" type="text" name="Email" placeholder="Email" value="${contactInfos ? contactInfos.email : ""}" />
               ${returnIcon("mail-outline")}
             </div>
-            <p id="phone-requested">Gültige Handynummer erforderlich</p>
-            <div class="input-container">
+            <p id="email-requested">Gültige Email-Adresse erforderlich</p>
+            </div>  
+            <div class="input-content">   
+            <div class="input-container" id="input-container-phone">
               <input id="number" type="text" name="Phone" placeholder="Phone" value="${contactInfos ? contactInfos.phone : ""}" />
               ${returnIcon("tel-outline")}
+            </div>
+            <p id="phone-requested">Gültige Handynummer erforderlich</p>
             </div>
             <div class="button-container">
               ${
@@ -67,30 +75,39 @@ export default function getContactOverlayTemplate(contactInfos) {
 
 function validateForm(fullName, email, phone) {
   const nameRequest = document.getElementById("name-requested");
+  const inputNameRequest = document.getElementById("input-container-name");
   let nameValidation = /^[a-zA-ZäöüÄÖÜß\s-]+$/;
   if (!nameValidation.test(fullName) || fullName.length < 10) {
     nameRequest.style.display = "block";
+    inputNameRequest.style.borderColor = "red";
     return false;
   } else {
     nameRequest.style.display = "none";
+    inputNameRequest.style.borderColor = "#d1d1d1";
   }
 
   const emailRequest = document.getElementById("email-requested");
+  const inputEmailRequest = document.getElementById("input-container-email");
   let emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailValidation.test(email)) {
     emailRequest.style.display = "block";
+    inputEmailRequest.style.borderColor = "red";
     return false;
   } else {
     emailRequest.style.display = "none";
+    inputEmailRequest.style.borderColor = "#d1d1d1";
   }
 
   const phoneRequest = document.getElementById("phone-requested");
+  const inputPhoneRequest = document.getElementById("input-container-phone");
   let phoneValidation = /^[0-9]+$/;
   if (!phoneValidation.test(phone) || phone.length < 10) {
     phoneRequest.style.display = "block";
+    inputPhoneRequest.style.borderColor = "red";
     return false;
   } else {
     phoneRequest.style.display = "none";
+    inputPhoneRequest.style.borderColor = "#d1d1d1";
   }
   return true;
 }
