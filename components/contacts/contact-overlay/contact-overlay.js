707a009fade7c6fb231ed9cf3a9a53b045d08b07
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-13 17:42:38 +0200

    Refactor contact-overlay.js: Update import and function calls

--- components/contacts/contact-overlay/contact-overlay.js
+++ components/contacts/contact-overlay/contact-overlay.js
@@ -1,122 +1,103 @@
 import returnIcon from "../../icons.js";
-import { handleDeleteContact } from "../contacts.js";
 import { editContact } from "../../firebase.js";
 import { renderContacts } from "../contacts.js";
 import { getInitialsFromName } from "../../utility-functions.js";
-import { addContact } from "../../firebase.js";
+import { addContact, deleteContact } from "../../firebase.js";
 import { showToast } from "../../toast/toast.js";
 
 export default function getContactOverlayTemplate(contactInfos) {
-  function handleEditContact(id, userColor) {
-    const fullName = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("number").value;
-    editContact(id, fullName, email, phone, userColor);
-    addContact(fullName, email, phone);
-    renderContacts();
-    showToast("Contact successfully Created" + returnIcon("check"));
-  }
-
   window.handleDeleteContact = handleDeleteContact;
   window.handleEditContact = handleEditContact;
   window.renderContacts = renderContacts;
-  window.validateForm = validateForm;
+  window.handleAddContact = handleAddContact;
   window.addContact = addContact;
 
   return /*html*/ `
-        <div class="edit-contact">
-      <div class="overlay-add-contact">
-        <div class="add-contact">
-          <div class="description">
-            <div class="join-container">
-              ${returnIcon("logo-light")}
-            </div>
-            <div class="description-box">
-              <h1>${contactInfos ? "Edit Contact" : "Add Contact"}</h1>
-               ${!contactInfos && "<p>Tasks are better with team!</p>"}
-              <div class="separator"></div>
-            </div>
-          </div>
+    <div class="edit-contact">
+      <div class="card">
+        <div class="header-container">
+          ${returnIcon("logo-light")}
+          <h1>${contactInfos ? "Edit Contact" : "Add Contact"}</h1>
+          ${!contactInfos && "<p>Tasks are better with team!</p>"}
+          <hr>
         </div>
-        <div class="person-container">
-          <div class="person-icon" style="background-color: ${contactInfos && "#" + contactInfos.userColor}">
-            ${contactInfos ? getInitialsFromName(contactInfos.fullName) : returnIcon("user-outline")}
-          </div>
-        </div>
-        <div class="input-container">
-          <div class="x-container" onclick="renderContacts()">
+        <div class="form-container">
+        <div class="close-container" onclick="renderContacts()">
             ${returnIcon("x")}
-          </div>
-          <form class="form" name="form-validation" action=""  >
-            <div class="form-name">
-              <input class="name-input" id="name" required type="text" name="Name" placeholder="Name" value="${contactInfos ? contactInfos.fullName : ""}" />
+        </div>
+        <div class="form">
+          <div>
+            <div class="bubble-container">
               ${returnIcon("user-outline")}
             </div>
-            <div class="form-email">
-              <input class="name-input" id="email" required type="text" name="Email" placeholder="Email" value="${contactInfos ? contactInfos.email : ""}" />
+          </div>
+          <div>
+            <div class="input-container">
+              <input id="name" type="text" name="name" placeholder="Name" value="${contactInfos ? contactInfos.fullName : ""}" />
+              ${returnIcon("user-outline")}
+            </div>
+            <div class="input-container">
+              <input id="email" type="text" name="Email" placeholder="Email" value="${contactInfos ? contactInfos.email : ""}" />
               ${returnIcon("mail-outline")}
             </div>
-            <div class="form-number">
-              <input class="name-input" id="number" required type="text" name="Phone" placeholder="Phone" value="${contactInfos ? contactInfos.phone : ""}" />
+            <div class="input-container">
+              <input id="number" type="text" name="Phone" placeholder="Phone" value="${contactInfos ? contactInfos.phone : ""}" />
               ${returnIcon("tel-outline")}
             </div>
-          </form>
-          <div class="button-container">
-            <div class="button-cancel-container">
-              ${
-                contactInfos
-                  ? `<button class="button-delete" onclick="handleDeleteContact(${contactInfos.id})">
-                Delete
-                ${returnIcon("trash-outline")}
-              </button>`
-                  : `<button class="button-cancel" onclick="renderContacts()">  
-                    Cancel ${returnIcon("x")} </button>`
-              } </div>
-            <div class="button-save-container">
-              ${
-                contactInfos
-                  ? `<button class="button-save" onclick="handleEditContact(${contactInfos.id}, '${contactInfos.userColor}')">
-                Save
-                ${returnIcon("check")}
-              </button>`
-                  : `<button class="button-save" onclick="addContact(${contactInfos.fullName}, '${contactInfos.email}', '${contactInfos.phone}')">  
-                    Create contact ${returnIcon("check")} </button>`
-              } </div>
+            <div class="button-container">
+              ${contactInfos ? `<button class="button-delete" onclick="handleDeleteContact('${contactInfos.id}')">Delete${returnIcon("trash-outline")}</button>` : `<button class="button-cancel" onclick="renderContacts()">Cancel ${returnIcon("x")} </button>`}
+              ${contactInfos ? `<button class="button-save" onclick="handleEditContact('${contactInfos.id}', '${contactInfos.userColor}')">Save${returnIcon("check")}</button>` : `<button class="button-save" onclick="handleAddContact()">Create contact ${returnIcon("check")} </button>`}
+            </div>
           </div>
         </div>
       </div>
-      <!-- <div class="button-save-container" >
-              <button class="button-save" onclick="handleEditContact(${contactInfos.id}, '${contactInfos.userColor}')">
-            <div class="button-save-container" >
-              <button id="save-btn" class="button-save" onclick="handleEditContact(${contactInfos.id}, '${contactInfos.userColor}')">
-                ${contactInfos ? "Save" : "Create Contact"}
-                ${returnIcon("check")}
-              </button>
-            </div>
-    </div> -->
-    `;
+    </div>`;
 }
 
-export function validateForm() {
-  let name = document.forms["form-validation"]["Name"].value;
+function validateForm(fullName, email, phone) {
   let nameValidation = /^[a-zA-ZäöüÄÖÜß\s-]+$/;
-  if (!nameValidation.test(name) || name.length < 10) {
+  if (!nameValidation.test(fullName) || fullName.length < 10) {
     alert("Ein vollständiger Name muss angegeben werden");
     return false;
   }
 
-  let email = document.forms["form-validation"]["Email"].value;
   let emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailValidation.test(email)) {
     alert("Eine gültige E-Mail Adresse muss angegeben werden");
     return false;
   }
 
-  let phone = document.forms["form-validation"]["Phone"].value;
   let phoneValidation = /^[0-9]+$/;
   if (!phoneValidation.test(phone) || phone.length < 10) {
     alert("Eine gültige Handynummer mit mind. 10 Ziffern muss angegeben werden");
     return false;
   }
   return true;
+}
+
+function handleAddContact() {
+  const fullName = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("number").value;
+
+  if (validateForm(fullName, email, phone)) {
+    addContact(fullName, email, phone);
+    renderContacts();
+    showToast("Contact successfully Created" + returnIcon("check"));
+  }
+}
+
+function handleEditContact(id, userColor) {
+  const fullName = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("number").value;
+  editContact(id, fullName, email, phone, userColor);
+  renderContacts();
+  showToast("Contact successfully Edited" + returnIcon("check"));
+}
+
+export function handleDeleteContact(id) {
+  deleteContact(id);
+  renderContacts();
+  showToast("Contact successfully Deleted" + returnIcon("check"));
 }
