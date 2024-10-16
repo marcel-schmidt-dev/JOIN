1718baf11c0d7b903253a2c041f0a9f4f3efe99e
Philipp Anstett <phil.anstett@icloud.com>
2024-10-16 12:53:26 +0200

    arrow-dropdown added

--- components/board/add-task/add-task.js
+++ components/board/add-task/add-task.js
@@ -1,10 +1,30 @@
-export default function getAddTaskTemplate() {
-  return /*html*/ `
+import returnIcon from "../../icons";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await getAddTaskTemplate();
+});
+
+async function getAddTaskTemplate() {
+  const addTaskRef = document.querySelector(".content");
+  addTaskRef.innerHTML += /*html*/ `
         <div class="content">
             <h1>Add Task</h1>
             <div class="input-container">
                 <div class="input-left">
-                    
+                    <div class="title-input">
+                        <h2>Title
+                            <span>*</span>
+                        </h2>
+                        <input type="text" placeholder="Enter a title">
+                    </div>
+                    <div class="description-input">
+                        <h2>Description</h2>
+                        <textarea type="text" placeholder="Enter a description" rows="5">
+                    </div>
+                    <div class="assigned-input">
+                        <h2>Assigned to</h2>
+                        <input type="text" id="selected-contact" placeholder="Select contacts to assign" readonly >
+                    </div>
                 </div>
             </div>
         </div>
