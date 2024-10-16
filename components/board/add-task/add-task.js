13db591546e7983de8adaed683338bde60b93fc6
Philipp Anstett <phil.anstett@icloud.com>
2024-10-16 11:59:20 +0200

    -add-task scss file added to style scss -started add-task template

---
+++ components/board/add-task/add-task.js
@@ -0,0 +1,12 @@
+export default function getAddTaskTemplate() {
+  return /*html*/ `
+        <div class="content">
+            <h1>Add Task</h1>
+            <div class="input-container">
+                <div class="input-left">
+                    
+                </div>
+            </div>
+        </div>
+    `;
+}
