a1899fc78744bf86c5e6c78d241d013313e1e177
Tobias Rothe <tobiasrothe77@gmail.com>
2024-10-16 09:46:16 +0200

    added To Do  Board

--- components/board/board.js
+++ components/board/board.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    await renderBoard();
+  await renderBoard();
 });
-
+document.addEventListener("DOMContentLoaded", async () => {
+  await getBoardTemplate();
+});
 async function renderBoard() {
-    const contactSectionRef = document.querySelector(".content");
+  const contactSectionRef = document.querySelector(".content");
 
-    contactSectionRef.innerHTML = /*html*/`
+  contactSectionRef.innerHTML = /*html*/ `
         <div class="board-container">
             <div class="heading">
                 <h2>Board</h2>
@@ -14,5 +16,47 @@ async function renderBoard() {
             </div>
             <div class="board"></div>
         </div>
-    `
+    `;
+}
+
+async function getBoardTemplate() {
+  const contactSectionRef = document.querySelector(".content");
+  contactSectionRef.innerHTML += /*html*/ `
+          <section class="slot-section">
+      <div class="slots">
+        <div class="slots-header">
+          <h2>To Do</h2>
+          <button class="btn">+</button>
+        </div>
+        <div class="slot-content"></div>
+      </div>
+
+      <div class="slots">
+        <div class="slots-header">
+          <div><h2>In Progress</h2></div>
+          <button class="btn">+</button>
+        </div>
+        <div class="slot-content" id="slot-content"></div>
+      </div>
+
+      <div class="slots">
+        <div class="slots-header">
+          <div>
+            <h2>Await feedback</h2>
+          </div>
+          <button class="btn">+</button>
+        </div>
+        <div class="slot-content"></div>
+      </div>
+
+      <div class="slots">
+        <div class="slots-header"><h2>Done</h2></div>
+        <div class="slot-content">
+          <p>test</p>
+          <p>test3</p>
+          <p>test5</p>
+        </div>
+      </div>
+    </section>
+    `;
 }
