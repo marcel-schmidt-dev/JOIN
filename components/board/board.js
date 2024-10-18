a0df8cc5b8bee6351a57dd8d1af96e7869b9e3ae
Tobias Rothe <tobiasrothe77@gmail.com>
2024-10-18 15:41:24 +0200

    added noTasks (placeholder)

--- components/board/board.js
+++ components/board/board.js
@@ -22,13 +22,10 @@ window.startDragging = function (taskId) {
   currentTaskId = taskId;
   const taskElement = document.querySelector(`[data-task-id="${taskId}"]`);
   taskElement.classList.add("rotate-task");
-  console.log(taskId);
 };
 
 window.highlight = function (taskId) {
   document.getElementById(taskId).classList.add("drag-area-highlight");
-
-  console.log(taskId);
 };
 
 window.removeAllHighlights = function () {
@@ -39,7 +36,17 @@ window.removeAllHighlights = function () {
 window.endDragging = function () {
   const taskElement = document.querySelector(`[data-task-id="${currentTaskId}"]`);
   taskElement.classList.remove("rotate-task");
+
   removeAllHighlights();
+};
+
+window.updatePlaceholder = function (slot) {
+  const placeholder = slot.querySelector(".placeholder");
+  if (slot.children.length > 1) {
+    placeholder.classList.add("d-none");
+  } else {
+    placeholder.classList.remove("d-none");
+  }
 };
 
 async function renderBoardTemplate() {
@@ -60,7 +67,7 @@ async function renderBoardTemplate() {
                   <h2>To do</h2>
                   <button class="btn">${returnIcon("plus")}</button>
                 </div>
-                <div class="slot-content" id="todo-tasks"  ondrop="moveTo('todo-tasks')"  ondragover="allowDrop(event); highlight('todo-tasks')"></div>
+                <div class="slot-content" id="todo-tasks"  ondrop="moveTo('todo-tasks')"  ondragover="allowDrop(event); highlight('todo-tasks')"><div class="placeholder"><p>No tasks To do</p></div></div>
               </div>
 
               <div class="slots">
@@ -68,7 +75,7 @@ async function renderBoardTemplate() {
                   <h2>In Progress</h2>
                   <button class="btn">${returnIcon("plus")}</button>
                 </div>
-                <div class="slot-content" id="inProgress-tasks" ondrop="moveTo('inProgress-tasks')"  ondragover="allowDrop(event); highlight('inProgress-tasks')" ></div>
+                <div class="slot-content" id="inProgress-tasks" ondrop="moveTo('inProgress-tasks')"  ondragover="allowDrop(event); highlight('inProgress-tasks')" ><div class="placeholder"><p>No tasks To do</p></div></div>
               </div>
 
               <div class="slots">
@@ -76,12 +83,14 @@ async function renderBoardTemplate() {
                   <h2>Await feedback</h2>
                   <button class="btn">${returnIcon("plus")}</button>
                 </div>
-                <div class="slot-content" id="awaitFeedback-tasks" ondrop="moveTo('awaitFeedback-tasks')"  ondragover="allowDrop(event); highlight('awaitFeedback-tasks')"></div>
+                <div class="slot-content" id="awaitFeedback-tasks" ondrop="moveTo('awaitFeedback-tasks')"  ondragover="allowDrop(event); highlight('awaitFeedback-tasks')">
+                  <div class="placeholder"><p>No tasks To do</p></div>
+                </div>
               </div>
 
               <div class="slots">
                 <div class="slots-header"><h2>Done</h2></div>
-                <div class="slot-content" id="done-tasks" ondrop="moveTo('done-tasks')" ondragover="allowDrop(event); highlight('done-tasks')"></div>
+                <div class="slot-content" id="done-tasks" ondrop="moveTo('done-tasks')" ondragover="allowDrop(event); highlight('done-tasks')"><div class="placeholder"><p>No tasks To do</p></div></div>
               </div>
             </div>
         </div>
