a25d411a17782e2937296352d67b6e91cf776a1c
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-28 12:17:05 +0100

    Refactor header component to add user menu functionality

--- components/header/header.js
+++ components/header/header.js
@@ -1,18 +1,40 @@
 import returnIcon from "../icons.js";
-import { checkAuthStatus, getAuthUser } from "../firebase.js";
+import { getAuthUser, signOutUser } from "../firebase.js";
 import { getInitialsFromName } from "../utility-functions.js";
+
+window.signOutUser = signOutUser;
+window.toggleMenu = toggleMenu;
 
 export default async function returnHeader() {
     const user = await getAuthUser();
-    const displayName = user ? user.displayName : 'Guest';
+    let displayName;
+    if (user) {
+        displayName = user.displayName ? user.displayName : 'Guest';
+    } else {
+        displayName = null;
+    }
+
     return /*html*/`
         <header>
             <span>Kanban Projekt Management Tool</span>
             ${returnIcon('logo-dark')}
             <div>
                 <a href="./help.html">${returnIcon('help')}</a>
-                <button>${getInitialsFromName(displayName)}</button>
+                ${displayName ? /*html*/ `
+                    <div class="user">
+                        <button onclick="toggleMenu(event)">${getInitialsFromName(displayName)}</button>
+                        <div class="user-menu">
+                            <a href="./legal-notice.html">Legal Notice</a>
+                            <a href="./privacy-policy.html">Privacy Policy</a>
+                            <span onclick="signOutUser()">Logout</span>
+                        </div>
+                    </div>`  : ''}
             </div>
         </header>
     `;
+}
+
+function toggleMenu(e) {
+    const userRef = e.target.parentElement;
+    userRef.classList.toggle('open');
 }
