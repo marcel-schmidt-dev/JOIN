d620abcfc01fd1a576a6e92c560d2fb8070bbf56
Philipp Anstett <phil.anstett@icloud.com>
2024-10-28 15:23:02 +0100

    deleteTask function at task-details implemented, not functional now

--- components/board/task-details/task-details.js
+++ components/board/task-details/task-details.js
@@ -1,18 +1,21 @@
 import { returnTaskById, getContact, returnSubTasks } from "../../firebase.js";
 import returnIcon from "../../icons.js";
 import { getInitialsFromName } from "../../utility-functions.js";
+import { deleteTask } from "../../firebase.js";
+
+window.deleteTask = deleteTask;
 
 export default async function showTaskDetails(taskId, slot) {
-    const task = await returnTaskById(taskId);
-    const contentRef = document.querySelector(".content");
-    const assignees = await Promise.all(task.assignee.map((id) => getContact(id)));
-    const subTasks = await returnSubTasks(taskId, slot);
+  const task = await returnTaskById(taskId);
+  const contentRef = document.querySelector(".content");
+  const assignees = await Promise.all(task.assignee.map((id) => getContact(id)));
+  const subTasks = await returnSubTasks(taskId, slot);
 
-    contentRef.innerHTML += /*html*/ `
+  contentRef.innerHTML += /*html*/ `
         <div class="task-details-container">
             <div class="task-details" data-task-id=${taskId} data-task-slot=${slot}>
                 <div class="top">
-                    <div class="type" style="background-color: ${task.type === 'Technical Task' ? '#1FD7C1' : '#0038FF'}">${task.type}</div>
+                    <div class="type" style="background-color: ${task.type === "Technical Task" ? "#1FD7C1" : "#0038FF"}">${task.type}</div>
                     <div class="close" onclick="closeTaskDetails()">X</div>
                 </div>
                 <h2>${task.title}</h2>
@@ -30,17 +33,29 @@ export default async function showTaskDetails(taskId, slot) {
                 
                 <div class="assignee-list">
                 <p>Assigned To:</p>
-                    ${assignees.map((assignee) => { return `<div class="assignee"><div class="bubble" style="background-color: #${assignee.userColor}">${getInitialsFromName(`${assignee.fullName}`)}</div><span>${assignee.fullName}</span></div>` }).join("")}
+                    ${assignees
+                      .map((assignee) => {
+                        return `<div class="assignee"><div class="bubble" style="background-color: #${assignee.userColor}">${getInitialsFromName(
+                          `${assignee.fullName}`
+                        )}</div><span>${assignee.fullName}</span></div>`;
+                      })
+                      .join("")}
                 </div>
 
                 <div class="subtask-list">
                     <p>Subtasks:</p>
-                    ${subTasks.map((subtask) => { return `<div class="subtask"><input type="checkbox" ${subtask.checked ? 'checked' : ''} name="${subtask.id}" id="${subtask.id}"><span>${subtask.title}</span></div>` }).join("")}
+                    ${subTasks
+                      .map((subtask) => {
+                        return `<div class="subtask"><input type="checkbox" ${subtask.checked ? "checked" : ""} name="${subtask.id}" id="${subtask.id}"><span>${
+                          subtask.title
+                        }</span></div>`;
+                      })
+                      .join("")}
                 </div>
                 <div class="buttons">
-                    <button>${returnIcon('trash-outline')}Delete</button>
+                    <button onclick="deleteTask('${slot}', '${taskId}')" >${returnIcon("trash-outline")}Delete</button>
                     <hr>
-                    <button>${returnIcon('pen')}Edit</button>
+                    <button>${returnIcon("pen")}Edit</button>
                 </div>
             </div>
         </div>
