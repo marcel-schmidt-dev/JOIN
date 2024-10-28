a1be4099facb1aa386f98c8d212ce602fd76b444
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-28 14:15:16 +0100

    Refactor DOMContentLoaded event listener in script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 import returnHeader from "./components/header/header.js";
 import returnSidebar from "./components/sidebar/sidebar.js";
 
-document.addEventListener('DOMContentLoaded', async function () {
-    const urlPath = window.location.pathname;
-    document.body.innerHTML += /*html*/`
-        <div class="app">
-            ${urlPath !== '/' && urlPath !== '/index.html' ? returnSidebar() : ''}
-            <div class="content-container">
-            ${urlPath !== '/' && urlPath !== '/index.html' ? await returnHeader() : ''}
-                <div class="content ${!urlPath !== '/' && !urlPath !== '/index.html' ? 'home-content' : ''}"></div>
-            </div>
-        </div>`;
-});
+const urlPath = window.location.pathname;
+document.body.innerHTML += /*html*/`
+    <div class="app">
+        ${urlPath !== '/' && urlPath !== '/index.html' ? returnSidebar() : ''}
+        <div class="content-container">
+        ${urlPath !== '/' && urlPath !== '/index.html' ? await returnHeader() : ''}
+            <div class="content ${!urlPath !== '/' && !urlPath !== '/index.html' ? 'home-content' : ''}"></div>
+        </div>
+    </div>`;
