f4dc86755cb8fceaa1179857ad7a27214f8ea93e
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-28 10:29:37 +0100

    added functionality to show the user initials

--- components/header/header.js
+++ components/header/header.js
@@ -1,14 +1,18 @@
-import returnIcon from "../icons.js"
+import returnIcon from "../icons.js";
+import { checkAuthStatus, getAuthUser } from "../firebase.js";
+import { getInitialsFromName } from "../utility-functions.js";
 
-export default function returnHeader() {
+export default async function returnHeader() {
+    const user = await getAuthUser();
+    const displayName = user ? user.displayName : 'Guest';
     return /*html*/`
         <header>
             <span>Kanban Projekt Management Tool</span>
             ${returnIcon('logo-dark')}
             <div>
                 <a href="./help.html">${returnIcon('help')}</a>
-                <button>G</button>
+                <button>${getInitialsFromName(displayName)}</button>
             </div>
         </header>
-    `
+    `;
 }
