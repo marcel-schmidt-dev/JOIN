02b6f527b99fae4f97e06a564d3d76d1b2418cf4
Marcel Schmidt <marcelschmidt1989@live.de>
2024-10-28 12:16:52 +0100

    Refactor getInitialsFromName function to log the full name for debugging purposes

--- components/utility-functions.js
+++ components/utility-functions.js
@@ -4,6 +4,8 @@ export function returnRandomUserColor() {
 }
 
 export function getInitialsFromName(fullName) {
+  console.log(fullName);
+
   const nameParts = fullName.split(" ");
   const firstNameInitial = nameParts[0].charAt(0).toUpperCase();
   const lastNameInitial = nameParts.length > 1 ? nameParts[1].charAt(0).toUpperCase() : "";
