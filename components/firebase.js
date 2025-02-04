db801850b2e90fa1813229e7e1ddf01dadb55684
Marcel Schmidt <marcelschmidt1989@live.de>
2025-02-04 16:33:27 +0100

    Refactor firebase.js for consistent string quoting and improved readability

--- components/firebase.js
+++ components/firebase.js
@@ -1,22 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  get,
-  push,
-  remove,
-  set,
-} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
-import { returnRandomUserColor, returnRandomContact } from "./utility-functions.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signInAnonymously,
-  createUserWithEmailAndPassword,
-  updateProfile,
-  signOut,
-} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
+import { getDatabase, ref, get, push, remove, set } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js';
+import { returnRandomUserColor, returnRandomContact } from './utility-functions.js';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInAnonymously, createUserWithEmailAndPassword, updateProfile, signOut } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
 
 /**
  * Initializes and returns the Firebase app, database, and authentication instance.
@@ -24,13 +9,13 @@ import {
  */
 function getFirebase() {
   const firebaseConfig = {
-    apiKey: "AIzaSyBDHRtmOAgzYOtLRS0haC7KvV_AQO-HodA",
-    authDomain: "join-d177e.firebaseapp.com",
-    databaseURL: "https://join-d177e-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "join-d177e",
-    storageBucket: "join-d177e.appspot.com",
-    messagingSenderId: "224091284746",
-    appId: "1:224091284746:web:22cd033dcc6f13c23146f3",
+    apiKey: 'AIzaSyBDHRtmOAgzYOtLRS0haC7KvV_AQO-HodA',
+    authDomain: 'join-d177e.firebaseapp.com',
+    databaseURL: 'https://join-d177e-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'join-d177e',
+    storageBucket: 'join-d177e.appspot.com',
+    messagingSenderId: '224091284746',
+    appId: '1:224091284746:web:22cd033dcc6f13c23146f3',
   };
 
   const app = initializeApp(firebaseConfig);
@@ -47,7 +32,7 @@ function getFirebase() {
  */
 export async function getContacts() {
   const { database } = getFirebase();
-  const contactsRef = ref(database, "contacts");
+  const contactsRef = ref(database, 'contacts');
 
   return get(contactsRef).then((snapshot) => {
     const contacts = [];
@@ -98,7 +83,7 @@ export function addContact(fullName, email, phone) {
   };
 
   const { database } = getFirebase();
-  const contactsRef = ref(database, "contacts");
+  const contactsRef = ref(database, 'contacts');
 
   const newContactRef = push(contactsRef, userObject);
   return newContactRef.key;
@@ -111,7 +96,7 @@ export function addContact(fullName, email, phone) {
 export async function deleteContact(id) {
   const { database } = getFirebase();
   const contactsRef = ref(database, `contacts/${id}`);
-  const boardSnap = await get(ref(database, "board/"));
+  const boardSnap = await get(ref(database, 'board/'));
 
   boardSnap.forEach((slot) => {
     slot.forEach((task) => {
@@ -140,8 +125,8 @@ export function editContact(id, name, email, phone, userColor) {
     email: email,
     phone: phone,
     userColor: userColor,
-  }).catch(error => {
-    console.error("Error updating contact:", error);
+  }).catch((error) => {
+    console.error('Error updating contact:', error);
   });
 }
 
@@ -154,7 +139,7 @@ export async function returnBoard(slot) {
   const { database } = getFirebase();
 
   try {
-    const snapshot = await get(ref(database, "board/"));
+    const snapshot = await get(ref(database, 'board/'));
     const board = snapshot.val();
 
     if (snapshot) {
@@ -230,16 +215,7 @@ export async function returnSubTasks(id, slot) {
  * @param {Array<string>} subTasks - An array of sub-task titles.
  * @param {Array<string>} assignee - An array of user IDs assigned to the task.
  */
-export function addTask(
-  slot = "todo",
-  title,
-  description,
-  type,
-  priority,
-  dueDate,
-  subTasks,
-  assignee
-) {
+export function addTask(slot = 'todo', title, description, type, priority, dueDate, subTasks, assignee) {
   const { database } = getFirebase();
   const taskRef = ref(database, `board/${slot}`);
 
@@ -277,17 +253,7 @@ export function deleteTask(slot, id) {
  * @param {Array<string>} subTasks - The updated sub-tasks of the task.
  * @param {Array<string>} assignee - The updated assignees for the task.
  */
-export function editTask(
-  slot,
-  id,
-  title,
-  description,
-  type,
-  priority,
-  dueDate,
-  subTasks,
-  assignee
-) {
+export function editTask(slot, id, title, description, type, priority, dueDate, subTasks, assignee) {
   const { database } = getFirebase();
   const taskRef = ref(database, `board/${slot}/${id}`);
   set(taskRef, {
@@ -310,14 +276,14 @@ export function editTask(
  */
 export async function moveTaskToSlot(newSlot, id) {
   try {
-    newSlot = newSlot.replace("-tasks", "");
+    newSlot = newSlot.replace('-tasks', '');
 
     const { database } = getFirebase();
     let taskRef = null;
     let taskData = null;
     let currentSlot = null;
 
-    const boardRef = ref(database, "board/");
+    const boardRef = ref(database, 'board/');
 
     const boardSnapshot = await get(boardRef);
     const board = boardSnapshot.val();
@@ -372,7 +338,7 @@ export async function updateSubTaskStatus(slot, taskId, subTaskId, isChecked, ti
   try {
     await set(subTaskRef, { checked: isChecked, title: title });
   } catch (error) {
-    console.error("Error updating subtask status:", error);
+    console.error('Error updating subtask status:', error);
     throw error;
   }
 }
@@ -385,40 +351,32 @@ export async function updateSubTaskStatus(slot, taskId, subTaskId, isChecked, ti
  */
 async function createRandomTasks() {
   const { database } = getFirebase();
-  const contactsRef = ref(database, "contacts/");
+  const contactsRef = ref(database, 'contacts/');
 
-  const exampleTitles = [
-    "Implement new feature",
-    "Fix bug in application",
-    "Write documentation",
-    "Design UI for new project",
-    "Conduct code review",
-  ];
+  const exampleTitles = ['Implement new feature', 'Fix bug in application', 'Write documentation', 'Design UI for new project', 'Conduct code review'];
   const exampleDescriptions = [
-    "This task involves implementing a new feature for the application.",
-    "This task is about fixing a bug that has been reported.",
-    "Document the code and create user manuals.",
-    "Design the user interface for the new project based on the requirements.",
-    "Review the code submitted by team members for quality assurance.",
+    'This task involves implementing a new feature for the application.',
+    'This task is about fixing a bug that has been reported.',
+    'Document the code and create user manuals.',
+    'Design the user interface for the new project based on the requirements.',
+    'Review the code submitted by team members for quality assurance.',
   ];
-  const examplePriorities = ["low", "medium", "urgent"];
-  const exampleDueDates = ["2024-11-01", "2024-11-15", "2024-11-30", "2024-12-15", "2025-01-01"];
-  const exampleTypes = ["Technical Task", "User Story"];
+  const examplePriorities = ['low', 'medium', 'urgent'];
+  const exampleDueDates = ['2024-11-01', '2024-11-15', '2024-11-30', '2024-12-15', '2025-01-01'];
+  const exampleTypes = ['Technical Task', 'User Story'];
 
   try {
     const contactsSnapshot = await get(contactsRef);
     const contacts = contactsSnapshot.val();
     const contactKeys = Object.keys(contacts);
 
-    const boardSlots = ["inProgress", "done", "awaitFeedback", "todo"];
+    const boardSlots = ['inProgress', 'done', 'awaitFeedback', 'todo'];
 
     for (const slot of boardSlots) {
       for (let i = 0; i < 2; i++) {
         const randomTitle = exampleTitles[Math.floor(Math.random() * exampleTitles.length)];
-        const randomDescription =
-          exampleDescriptions[Math.floor(Math.random() * exampleDescriptions.length)];
-        const randomPriority =
-          examplePriorities[Math.floor(Math.random() * examplePriorities.length)];
+        const randomDescription = exampleDescriptions[Math.floor(Math.random() * exampleDescriptions.length)];
+        const randomPriority = examplePriorities[Math.floor(Math.random() * examplePriorities.length)];
         const randomDueDate = exampleDueDates[Math.floor(Math.random() * exampleDueDates.length)];
         const randomType = exampleTypes[Math.floor(Math.random() * exampleTypes.length)];
 
@@ -436,8 +394,8 @@ async function createRandomTasks() {
           priority: randomPriority,
           dueDate: randomDueDate,
           subTasks: [
-            { title: "Initial setup", checked: false },
-            { title: "Create documentation", checked: false },
+            { title: 'Initial setup', checked: false },
+            { title: 'Create documentation', checked: false },
           ],
           assignee: randomAssignees,
         };
@@ -446,9 +404,9 @@ async function createRandomTasks() {
       }
     }
 
-    console.log("Random tasks created in each slot");
+    console.log('Random tasks created in each slot');
   } catch (error) {
-    console.error("Error creating tasks: ", error);
+    console.error('Error creating tasks: ', error);
   }
 }
 
@@ -483,7 +441,7 @@ export async function getAuthUser() {
 export async function checkAuth() {
   const user = await getAuthUser();
   if (!user) {
-    window.location.href = "/index.html";
+    window.location.href = '/index.html';
   }
 }
 
@@ -516,9 +474,9 @@ export async function signOutUser() {
   const { auth } = getFirebase();
   try {
     await signOut(auth);
-    window.location.href = "/";
+    window.location.href = '/';
   } catch (error) {
-    console.error("Error signing out:", error);
+    console.error('Error signing out:', error);
     throw error;
   }
 }
@@ -533,7 +491,7 @@ export async function signInAnonymouslyUser() {
   const { auth } = getFirebase();
   try {
     const userCredential = await signInAnonymously(auth);
-    userCredential.user.displayName = "Guest";
+    userCredential.user.displayName = 'Guest';
 
     return userCredential.user;
   } catch (error) {
